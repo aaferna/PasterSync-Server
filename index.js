a53e789e0618f30d1bec426720321eccc570f782
Agustin Fernandez <agustin.alejandro-fernandez@externo-ar.cencosud.com>
2021-07-20 12:55:01 -0300

    0.1.1

--- index.js
+++ index.js
@@ -3,19 +3,13 @@ const fs = require('fs');
 const unzipper = require('unzipper');
 const fileUpload = require('express-fileupload');
 const app = express();
+const conf = require('./config.json');
 
+let finaldir = conf.troncal
+let tmp = conf.temporal
 
-let tempFileDir = './tmp/'
-let finaldir = './pre/'
-
-// default options
-app.use(fileUpload({
-    useTempFiles : true,
-    tempFileDir: tempFileDir
-}));
-
+app.use(fileUpload({ useTempFiles : true, tempFileDir: tmp }));
 app.set('json spaces', 2);
-
 
 app.post('/upload', function(req, res) {
 
@@ -23,41 +17,43 @@ app.post('/upload', function(req, res) {
     const time = hoy.getFullYear()+'/'+hoy.getMonth()+'/'+hoy.getDay()+'/';
 
     if (!req.files || Object.keys(req.files).length === 0) {
-        return res.status(400).send('No files were uploaded.');
+        return res.status(400).json({ status : "No hay Archivo" });
     }
-
 
     try {
 
         let FilPosted = req.files;
-        FilPosted.data.mv(finaldir+FilPosted.data.name, function(req, res) {
+        FilPosted.data.mv(tmp+FilPosted.data.name, function(req, res) {
 
             let arrDirs = FilPosted.data.name.replace('.zip', '').split('_').toString().replace(',', "//")
 
-             fs.createReadStream(finaldir+FilPosted.data.name)
-             .pipe(
-                 unzipper.Extract(
-                     { 
-                         path: time+arrDirs 
-                     }
-                 )
-             );
-             fs.unlinkSync(finaldir+FilPosted.data.name)
+            if(!arrDirs) { return res.status(400).json({ status : "No Archivo" }); }
+            else {
 
+                fs.createReadStream(tmp+FilPosted.data.name)
+                .pipe(
+                    unzipper.Extract(
+                        { 
+                            path: finaldir+time+arrDirs 
+                        }
+                    )
+                );
+
+                fs.unlinkSync(tmp+FilPosted.data.name)
+
+            }
+        
         })
         
         res.json({ status : "OK" });
 
     } catch (error) {
-
         console.log(error)
         res.json({ status : "FAIL", msj: JSON.stringify(error) });
     }
  
 });
 
-
-
 app.listen("3000", () => {
-    console.log(`Example app listening at http://localhost:3000`)
+    console.log(`Sirviendo http://localhost:3000`)
 })
