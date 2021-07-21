6fc5208272d4ad39afe3e169a38752f537d8c7a6
Agustin Fernandez <aaferna@outlook.com.ar>
2021-07-20 23:45:48 -0300

    2.0.0

--- index.js
+++ index.js
@@ -1,15 +1,20 @@
 const express = require('express');
+const cors = require('cors');
 const fs = require('fs');
-const unzipper = require('unzipper');
+const AdmZip = require('adm-zip');
 const fileUpload = require('express-fileupload');
 const app = express();
 const conf = require('./config.json');
 
-let finaldir = conf.troncal
+let troncal = conf.troncal
 let tmp = conf.temporal
 
 app.use(fileUpload({ useTempFiles : true, tempFileDir: tmp }));
 app.set('json spaces', 2);
+    	app.set(cors({
+        origin: '*'
+    }));
+
 
 app.post('/upload', function(req, res) {
 
@@ -19,29 +24,52 @@ app.post('/upload', function(req, res) {
     if (!req.files || Object.keys(req.files).length === 0) {
         return res.status(400).json({ status : "No hay Archivo" });
     }
-
     try {
 
         let FilPosted = req.files;
-        FilPosted.data.mv(tmp+FilPosted.data.name, function(req, res) {
+            FilPosted.data.mv(tmp+FilPosted.data.name, function(req, res) {
 
-            let arrDirs = FilPosted.data.name.replace('.zip', '').split('_').toString().replace(',', "//")
+            let testFile = FilPosted.data.name.split('.zip')
 
-            if(!arrDirs) { return res.status(400).json({ status : "No Archivo" }); }
-            else {
+            if(testFile.length == 2){ 
+                console.log("Es zip")
+                let arrDirs = FilPosted.data.name.replace('.zip', '')
+                let preform = arrDirs.split('_')
+                let finaldir = ""
+                preform.map(el=>{
+                    finaldir = finaldir + el + "/"
+                })
+                console.log(finaldir)
+               
+                if(!arrDirs) { return res.status(400).json({ status : "No Archivo" }); }
+                else {
+                    console.log('aca',tmp+FilPosted.data.name, troncal+time+finaldir )
+                   
+                    try{
+                        let file = tmp+FilPosted.data.name
+                        let destino = troncal+time+finaldir
+                        var zip = new AdmZip(file);
+                       // zipEntries = zip.getEntries();
+                        zip.extractAllTo(destino, /*overwrite*/true);
+    
+                    } catch(err){
+                        console.log(err)
+                    }
 
-                fs.createReadStream(tmp+FilPosted.data.name)
-                .pipe(
-                    unzipper.Extract(
-                        { 
-                            path: finaldir+time+arrDirs 
-                        }
-                    )
-                );
+                    // fs.createReadStream(tmp+FilPosted.data.name)
+                    // .pipe(
+                    //     unzipper.Extract(
+                    //         { 
+                    //             path: troncal+time+finaldir 
+                    //         }
+                    //     )
+                    // );
+                 //    fs.unlinkSync(tmp+FilPosted.data.name)
+                }
 
-                fs.unlinkSync(tmp+FilPosted.data.name)
+            } else { console.log("No es un archivo ZIP") }
 
-            }
+           
         
         })
         
